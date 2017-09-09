dbf831f9af157898c4df1e247e0939da7882424a
Administrator <Administrator@192.168.0.18>
2017-09-09 12:16:31 +0800

    初始化导入

---
+++ src/main/java/org/xplus/sample/XplusSbSampleOmuiApplication.java
@@ -0,0 +1,12 @@
+package org.xplus.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XplusSbSampleOmuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XplusSbSampleOmuiApplication.class, args);
+	}
+}
